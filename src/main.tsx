e8d3ecadcc1f94c1657c02e2ffbaa2a20f5116ce
Sina Khaninejad <sinakaya98765@gmail.com>
2023-06-14 16:22:07 +0330

    fix browser router

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter, HashRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
     <App />
-  </brow>
+  </BrowserRouter>
 );
 
 // Switch to the HashRouter since GitHub pages doesn't support the tech used by the BrowserRouter.
